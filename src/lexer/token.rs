7743ebd0ad192d1da16b3ce7b45778101ad25e02
Sam Thomas <>
2022-01-02 14:24:50 +0000

    lexer.rs: Added boolean for binding modifier Added a bool to the Token struct to denote whether the token's binding power has been modified

--- src/lexer/token.rs
+++ src/lexer/token.rs
@@ -3,12 +3,19 @@ use crate::pos::Span;
 /// The Token created by the Lexer and used by the Parser to generate an AST
 pub struct Token<'a> {
     token: TokenKind<'a>,
+    /// This field is necessary to denote the possible addition of the Binding Modifier Operator.
+    /// This is the `$` operator and is used to boost the precedence / binding power of the token.
+    /// 
+    /// This allows for expressions such as `10 > 5 ? 10 : 20 +$ 30` to evealuate to 40 (whereas without the binding modifier, it would have evaluated to 10).
+    /// 
+    /// This cannot be applied / won't have an effect on certain tokens such as literals, newlines, identifiers or keywords.
+    binding_modified: bool,
     span: Span,
 }
 
 impl<'a> Token<'a> {
-    pub fn new(token: TokenKind<'a>, span: Span) -> Self {
-        Self { token, span }
+    pub fn new(token: TokenKind<'a>, binding_modified: bool, span: Span) -> Self {
+        Self { token, binding_modified, span }
     }
 }
 
