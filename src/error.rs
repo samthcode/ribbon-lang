ee539d91588ebcaeed4bd025d778435d32ad06b5
Sam Thomas <>
2022-01-18 21:21:44 +0000

    eprint_error_message function which prints the errors really nicely similar to rusts error system (but currently without colours)

--- src/error.rs
+++ src/error.rs
@@ -17,3 +17,18 @@ impl std::fmt::Display for Error {
         write!(f, "ERROR: {}: {}", self.span, self.message)
     }
 }
+
+pub fn eprint_error_message(errors: Vec<Error>, file_name: &str, source: String) {
+    eprintln!("File {file_name} failed to compile with {} error{}:", errors.len(), if errors.len() == 1 {""} else {"s"});
+
+    for error in errors {
+        eprintln!();
+        eprintln!("error: {file_name}:{}: {}", error.span.start, error.message);
+        if error.span.start.line == error.span.end.line {
+            let col_len = error.span.start.line.to_string().len();
+            eprintln!("{} |", " ".repeat(col_len));
+            eprintln!("{} | {}", error.span.start.line, source.lines().nth(error.span.start.line - 1).unwrap_or_else(|| panic!("Ribbon internal error printing errors! Whoops! Kinda ironic lol")));
+            eprintln!("{} | {}{}", " ".repeat(col_len), " ".repeat(error.span.start.col - 1), "^".repeat(std::cmp::max(1, error.span.end.col - error.span.start.col)));
+        }
+    }
+}
