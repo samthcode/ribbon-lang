0a9e37aaedf0f9af4fa2662cf007a79735792e7f
samthcode <samuelbrynnethomas@icloud.com>
2025-11-20 17:23:50 +0000

    Rough precedences. These will absolutely change as I figure out what I'm doing.

--- ribbon_parser/src/prec.rs
+++ ribbon_parser/src/prec.rs
@@ -1,63 +1,77 @@
 use crate::lexer;
 use lexer::tok::TokKind;
 
-pub fn unary_prec(kind: &TokKind) -> u8 {
-    match kind {
-        TokKind::Ident(_) => todo!(),
-        TokKind::Kw(kw_kind) => todo!(),
-        TokKind::Lit(lit_kind) => todo!(),
-        TokKind::Op(op_kind) => todo!(),
-    }
+pub enum Fixity {
+    Left,
+    Right,
+    None,
 }
 
-pub fn binary_prec(kind: &TokKind) -> u8 {
+pub fn unary_prec(kind: &TokKind) -> u8 {
     use lexer::OpKind::*;
     match kind {
         TokKind::Ident(_) => todo!(),
-        TokKind::Kw(kw_kind) => todo!(),
-        TokKind::Lit(lit_kind) => todo!(),
+        TokKind::Kw(_) => todo!(),
+        TokKind::Lit(_) => todo!(),
         TokKind::Op(op_kind) => match op_kind {
-            Plus | Minus => todo!(),
-            Mul | Div => todo!(),
-            Mod => todo!(),
-            LParen => todo!(),
-            RParen => todo!(),
-            LSquare => todo!(),
-            RSquare => todo!(),
-            LCurly => todo!(),
-            RCurly => todo!(),
-            Dot => todo!(),
-            Colon => todo!(),
+            // Unary minus/not/deref/borrow
+            Minus | Bang | Mul | Amp => 15,
+            // This shouldn't be called on anything else
+            // Need an error system set up in the parser first
+            _ => todo!(),
+        },
+    }
+}
+
+fn binary_prec(kind: &TokKind) -> (u8, Fixity) {
+    use Fixity::*;
+    use lexer::OpKind::*;
+    match kind {
+        TokKind::Op(op_kind) => match op_kind {
+            Path => (19, None),
+            // Method call/field expression/no-parenthesis method call/(non-method) tilde call
+            Dot | Colon | Tilde => (18, Left),
+            // Error propagation
+            TildeQuestion => (17, Left),
+            // Function call/array indexing
+            LParen | LSquare => (16, None),
+            // -- Gap for unary ops --
+            Mul | Div | Mod => (14, Left),
+            Plus | Minus => (13, Left),
+            ShiftL | ShiftR => (12, Left),
+            Amp => (11, Left),
+            Caret => (10, Left),
+            Pipe => (9, Left),
+            // Comparisons
+            Lt | Gt | EqEq | BangEq | LtEq | GtEq => (8, Left),
+            And => (7, Left),
+            Or => (6, Left),
+            // Ranges
+            DotDot | DotDotEq => (5, None),
+            // Piping operators
+            ColonGt | TildeGt => (4, Left),
+            // All assignments
+            Eq | AmpEq | CaretEq | PipeEq | PlusEq | MinusEq | MulEq | DivEq | ModEq | DotEq
+            | ShiftLEq | ShiftREq | AndEq | OrEq => (3, Right),
+            // Block
+            LCurly | RCurly => (2, None),
+            // Argument list terminator
+            RParen => (1, None),
+            // List terminator
+            RSquare => (0, None),
+
+            // Not sure what to do with these yet
             Semi => todo!(),
             At => todo!(),
             Hash => todo!(),
-            Tilde => todo!(),
-            Amp => todo!(),
-            Pipe => todo!(),
             Bang => todo!(),
-            Eq | AmpEq | PipeEq | PlusEq | MinusEq | MulEq | DivEq | ModEq | DotEq | ShiftLEq
-            | ShiftREq => todo!(),
             Dollar => todo!(),
-            Lt => todo!(),
-            Gt => todo!(),
-            EqEq => todo!(),
-            BangEq => todo!(),
-            LtEq => todo!(),
-            GtEq => todo!(),
-            And => todo!(),
-            Or => todo!(),
-            Path => todo!(),
-            DotDot => todo!(),
-            ColonGt => todo!(),
-            TildeGt => todo!(),
             MinusGt => todo!(),
             EqGt => todo!(),
-            TildeQuestion => todo!(),
-            ShiftL => todo!(),
-            ShiftR => todo!(),
-            AndEq => todo!(),
-            OrEq => todo!(),
-            DotDotEq => todo!(),
         },
+        // binary_prec shouldn't be called with these afaik
+        TokKind::Ident(_) => todo!(),
+        TokKind::Kw(_) => todo!(),
+        TokKind::Lit(_) => todo!(),
     }
 }
