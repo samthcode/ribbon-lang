e74a90e47af004f170733ac6abaf1ece2a7e002d
samthcode <samuelbrynnethomas@icloud.com>
2025-11-25 01:34:58 +0000

    Remove lexer test from main.rs, we likely don't need it.

--- ribbon/src/main.rs
+++ ribbon/src/main.rs
@@ -1,11 +1,6 @@
-use ribbon_lexer::Lexer;
 use ribbon_parser::Parser;
 
 fn main() {
-    let lexer = Lexer::new("const main := () -> { \"Hello World\" }");
-    for i in lexer {
-        println!("{}", i)
-    }
     let parser = Parser::new("[1+3,");
     let program = parser.parse();
     if !program.1.is_empty() {
